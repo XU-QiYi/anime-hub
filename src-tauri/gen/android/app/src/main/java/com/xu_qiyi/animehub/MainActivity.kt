7ce163ca7074ed3ae246e5b7281a5ab6563ebc25
AccountingUser <qugene414@gmail.com>
2026-05-11 21:48:34 +0800

    feat: APK构建完成（签名+Android项目文件）

---
+++ src-tauri/gen/android/app/src/main/java/com/xu_qiyi/animehub/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xu_qiyi.animehub
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
